32a3add5c55b564f89ca9338fdc3af1fa5e93a3a
Endrit <prenkuendrit@gmail.com>
2023-03-29 17:38:31 +0200

    coded spinner while it fetches the data from the api

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
 	<main class="bg-neutral-200 dark:bg-slate-900 min-h-screen">
 	  <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-		<transition
-		  enter-from-class="opacity-0"
-		  leave-to-class="opacity-0"
-		  enter-active-class="transition duration-300"
-		  leave-active-class="transition duration-300"
-		>
-		  <router-view></router-view>
-		</transition>
+		<router-view></router-view>
 	  </div>
 	</main>
   </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,10 @@
 	  </section>
   
 	  <section class="section bg-slate-50 dark:bg-slate-800">
-		<div class="flex flex-col justify-center h-full">
+		<div
+		  :class="isLoading ? 'opacity-30' : ''"
+		  class="flex flex-col justify-center h-full"
+		>
 		  <strong
 			class="text-2xl inline-block mb-4 text-center text-slate-900 dark:text-slate-200"
 		  >
@@ -56,7 +59,7 @@
 	  <section class="mb-6">
 		<h1 class="text-xl mb-3 text-slate-900 dark:text-slate-200">
 		  <i class="fa fa-chevron-right"></i>
-		  Routes
+		  Menu
 		</h1>
 		<div class="flex gap-4">
 		  <template v-for="(route, i) in routes" :key="i">
@@ -67,56 +70,80 @@
 			>
 		  </template>
 		</div>
+		<div
+		  role="status"
+		  v-if="isLoading"
+		  class="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+		>
+		  <svg
+			aria-hidden="true"
+			class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+			viewBox="0 0 100 101"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		  >
+			<path
+			  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+			  fill="currentColor"
+			/>
+			<path
+			  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+			  fill="currentFill"
+			/>
+		  </svg>
+		</div>
 	  </section>
 	  <Footer />
 	</main>
   </template>
   
   <script setup>
-  import { ref, onMounted } from 'vue'
-  import { useRouter } from 'vue-router'
-  import Footer from '@/components/Footer.vue'
-  import DarkBtn from '@/components/DarkBtn.vue'
+   import { ref, onMounted } from 'vue'
+   import { useRouter } from 'vue-router'
+   import Footer from '@/components/Footer.vue'
+   import DarkBtn from '@/components/DarkBtn.vue'
   
-  const router = useRouter()
+   const router = useRouter()
   
-  const movePage = to => router.push({ name: to })
+   const movePage = to => router.push({ name: to })
   
-  const routes = [
-	{
-	  name: 'About',
-	  to: 'About'
-	},
-	{
-	  name: 'Projects',
-	  to: 'Projects'
-	}
-  ]
-  
-  const quote = ref('')
-  const author = ref('')
-  
-  const apiUrl = `https://api.api-ninjas.com/v1/quotes?category=inspirational`
-  const apiKey = 'gRFjZMpCCqvtIz5GsUVh5w==F2nuJJtPbLqVbZkm'
-  
-  async function fetchQuotes() {
-	try {
-	  const response = await fetch(apiUrl, {
-		headers: {
-		  'X-Api-Key': apiKey,
-		  'Content-Type': 'application/json',
-		},
-	  });
-  
-	  const data = await response.json();
-	  quote.value = data[0].quote;
-	  author.value = data[0].author;
-	} catch (error) {
-	  console.error('Error: ', error);
-	}
+   const routes = [
+  {
+	name: 'About',
+	to: 'About'
+  },
+  {
+	name: 'Projects',
+	to: 'Projects'
   }
+   ]
   
-  onMounted(fetchQuotes)
+   const quote = ref('')
+   const author = ref('')
+   const isLoading = ref(true)
+  
+   const apiUrl = `https://api.api-ninjas.com/v1/quotes?category=inspirational`
+   const apiKey = 'gRFjZMpCCqvtIz5GsUVh5w==F2nuJJtPbLqVbZkm'
+  
+   async function fetchQuotes() {
+  try {
+	const response = await fetch(apiUrl, {
+   headers: {
+  'X-Api-Key': apiKey,
+  'Content-Type': 'application/json',
+   },
+	});
+  
+	const data = await response.json();
+	quote.value = data[0].quote;
+	author.value = data[0].author;
+	isLoading.value = false;
+  } catch (error) {
+	console.error('Error: ', error);
+  }
+   }
+  
+   onMounted(fetchQuotes)
   </script>
   
   <style scoped>
